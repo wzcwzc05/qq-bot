30e52d619ddbc717b32176d27d9038c446331ceb
wzcwzc05 <41479572+wzcwzc05@users.noreply.github.com>
2022-03-28 15:29:42 +0800

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,14 +1,10 @@
 import requests
 import json
 
-with open("./port.txt", 'r', encoding='utf-8') as file:
-        http_port = int(file.read())
-address = "http://127.0.0.1:" + str(http_port)
 
-def keyword(http_port,message, uid, gid=None):
+def keyword(http_port, message, uid, gid=None):
     address = "http://127.0.0.1:" + str(http_port)
     if message[0:4] == 'ping':
         if (gid == None):
             requests.get(
-                url='http://127.0.0.1:5700/send_private_msg?user_id={0}&message={1}'.format(uid, "别叫了，我在"))
-    if (message[0:])
+                url = address + '/send_private_msg?user_id={0}&message={1}'.format(uid, "别叫了，我在"))
