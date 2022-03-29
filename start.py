335e2376ff29a88dc069725d611663ceda66797f
wzcwzc05 <41479572+wzcwzc05@users.noreply.github.com>
2022-03-29 19:01:16 +0800

    Update start.py

--- start.py
+++ start.py
@@ -4,17 +4,13 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read('./main.ini')
-if (os.path.exists("./config.yml")):
+if (os.path.exists("./go-cqhttp/config.yml")):
     print("Find go-cqhttp Configuration......")
 else:
     print("No go-cqhttp Configuration......")
     os.system("pause")
     exit()
-yamlPath = "./config.yml"
-# with open(address, 'w', encoding='utf-8') as write_file:
-#    http_port = str(input("Enter your HTTP port:"))
-#    write_file.write("127.0.0.1:" + http_port)
-
+yamlPath = "./go-cqhttp/config.yml"
 
 with open(yamlPath, 'r', encoding='utf-8') as file:
     result = file.read()
@@ -23,7 +19,6 @@ with open(yamlPath, 'r', encoding='utf-8') as file:
     qq_num = int(input("Enter your QQ Number:"))
     qq_passwd = str(
         input("Enter your QQ password(if is empty then will scan the QRcode):"))
-#    http_port = int(input("Enter your go-cqhttp port:"))
     http_port = config.getint('go-cqhttp', 'port')
     flask_port = config.getint('flask', 'port')
     print("For more go-cqhttp Configuration, Edit the ./config.yml ......")
@@ -37,4 +32,4 @@ with open(yamlPath, 'r', encoding='utf-8') as file:
         yaml.dump(bot_yml, write_file)
 
 print("Starting go-cqhttp Server on port......")
-os.system("./go-cqhttp/go-cqhttp fastboot")
+os.system("cd go-cqhttp && ./go-cqhttp fastboot")
