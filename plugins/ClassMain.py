3870c081b2ac7fb8b72a47b31a16e75659d03e70
wzcwzc05 <wzcwzc0@outlook.com>
2022-07-18 21:01:24 +0800

    Update

--- plugins/ClassMain.py
+++ plugins/ClassMain.py
@@ -1,15 +1,19 @@
 import requests
 from plugins import api
 
+
 class MessageEvent():
     IsGroup = False
     IsPrivate = False
     Message = ""
     uid = ""
-    def __init__(self,IsGroup,IsPrivate,Message,uid) -> None:
-        self.IsGroup=IsGroup
-        self.IsPrivate=IsPrivate
-        self.Message=Message
-        self.uid=uid
 
-      
+    def __init__(self, Http_Port, IsGroup, IsPrivate, Message, uid) -> None:
+        self.Http = Http_Port
+        self.IsGroup = IsGroup
+        self.IsPrivate = IsPrivate
+        self.Message = Message
+        self.uid = uid
+
+    def MessageDeal(self):
+        raise NotImplementedError("Not implemented!")
--- plugins/test/main.py
+++ plugins/test/main.py
@@ -1,2 +1,9 @@
-def ping():
-    return "pong"
+from plugins import *
+
+
+class test(ClassMain.MessageEvent):
+    def __init__(self, Http_Port, IsGroup, IsPrivate, Message, uid) -> None:
+        super().__init__(Http_Port, IsGroup, IsPrivate, Message, uid)
+
+    def MessageDeal(self):
+        return super().MessageDeal()
